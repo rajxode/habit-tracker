bec920e83e64be8358a9c40a4567b0d4e854330f
rajxode <rajsh1202@gmail.com>
2023-08-24 12:39:10 +0530

    add comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import Navbar from "./Component/Navbar";
-import Homepage from "./Pages/Homepage";
-import DetailsPage from "./Pages/DetailsPage";
-import { Error } from "./Pages/Error";
+
+// react-router-dom
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+// importing all the required components and pages
+// navbar 
+import Navbar from "./Component/Navbar";
+// the main homepage
+import Homepage from "./Pages/Homepage";
+// details page for showing habit stats
+import DetailsPage from "./Pages/DetailsPage";
+// error page in case of error
+import { Error } from "./Pages/Error";
+
+
+// to render the habit tracker app
 function App() {
 
+  // defining different routes 
   const router = createBrowserRouter([
     {
+      // default path 
       path:"/",
       element: <Navbar />,
       errorElement: <Error />,
       children:[
+        // to render the homepage
         {index:true, element: <Homepage />},
+        // for rendering the detailspage
         {path:"/detailspage", element: <DetailsPage />},
       ]
     }
   ]);
 
+
   return (
+    // rendering the page based on routes
     <RouterProvider router={router} />
   );
 }
 
+// export the component
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// style file
 import './index.css';
+
+// app component
 import App from './App';
+// redux store provider
 import { Provider } from 'react-redux';
+// redux store
 import { store } from './Redux/Store/store';
 
+// for toast notification
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* redux store */}
     <Provider store={store}>
       {/* for toast notification */}
       <ToastContainer />
+      {/* render the app */}
       <App />
     </Provider>
   </React.StrictMode>
